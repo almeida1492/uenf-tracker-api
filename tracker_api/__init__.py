ef35d1bca8a4212ff31b616d88ca164bf443269e
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-09-23 14:53:02 +0200

    fix route import at init

--- tracker_api/__init__.py
+++ tracker_api/__init__.py
@@ -3,13 +3,13 @@ from flask import Flask
 from .commands import create_tables
 from .extensions import db
 from .models import Location
-import server
+from .routes import main
 
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
     db.init_app(app)
-    app.register_blueprint(server)
+    app.register_blueprint(main)
     app.cli.add_command(create_tables)
     return app
